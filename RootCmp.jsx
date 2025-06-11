697931fa23d1e1139936430a4be38085e8b57abf
Idan <idan.h@squaretalk.com>
2025-06-11 23:53:15 +0300

    Added routing

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,3 +1,24 @@
+const Router = ReactRouterDOM.HashRouter
+const { Routes, Route, Navigate } = ReactRouterDOM
+
+import { HomePage } from "./cmps/HomePage.jsx"
+import { AboutUs } from "./cmps/AboutUs.jsx"
+import { BookIndex } from "./cmps/BookIndex.jsx"
+import { BookDetails } from "./cmps/BookDetails.jsx"
+import { BookEdit } from "./cmps/BookEdit.jsx"
+
 export function RootCmp() {
-	return <section className="main">Hello world!</section>
+	return (
+		<Router>
+			<section className="main">
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/about" element={<AboutUs />} />
+					<Route path="/book" element={<BookIndex />} />
+					<Route path="/book/:bookId" element={<BookDetails />} />
+					<Route path="/book/edit" element={<BookEdit />} />
+				</Routes>
+			</section>
+		</Router>
+	)
 }
---
+++ cmps/BookIndex.jsx
@@ -0,0 +1,3 @@
+export function BookIndex() {
+	return <div>BooxIndex!</div>
+}
